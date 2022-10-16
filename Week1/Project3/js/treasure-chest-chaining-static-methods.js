b1cf6f7dc26823ef05799f37ec851e4aa900a901
Dave Letorey <dave@code-red.uk>
2022-10-16 12:38:41 +0100

    added zero to the amount array so it is possible to return 0 as a value

--- Week1/Project3/js/treasure-chest-chaining-static-methods.js
+++ Week1/Project3/js/treasure-chest-chaining-static-methods.js
@@ -49,7 +49,7 @@ const smBronzeVal = document.querySelector("#smBronzeVal")
 const smLootVal = document.querySelector("#smLootVal")
 buttons.addEventListener("click", addTreasure)
 const TreasureChest = (function () {
-  const amount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+  const amount = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
   /**
    * Randomly shuffle an array
    * https://stackoverflow.com/a/2450976/1293256
